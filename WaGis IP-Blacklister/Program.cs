f8b7f701af93a794ee18f757c4431133ae6fdc8c
WaGi <wagi.coding@gmail.com>
2019-09-27 07:47:53 +0200

    Added mutex to allow only one instance running

--- WaGis IP-Blacklister/Program.cs
+++ WaGis IP-Blacklister/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Principal;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -9,10 +10,11 @@ namespace WaGis_IP_Blacklister
 {
     static class Program
     {
+        private static Mutex _mutex = null;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
+        [STAThread]        
         static void Main()
         {
 
@@ -21,6 +23,14 @@ namespace WaGis_IP_Blacklister
                 WindowsIdentity identity = WindowsIdentity.GetCurrent();
                 WindowsPrincipal principal = new WindowsPrincipal(identity);
                 return principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+            _mutex = new Mutex(true, @"Global\" + "WaGi-IP-Blacklister", out bool aIsNewInstance);
+            GC.KeepAlive(_mutex);
+
+            if (!aIsNewInstance)
+            {
+                MessageBox.Show("An instance of WaGi-IP-Blacklister is already running!\nIf you can't find the opened instance, checkout the System Tray or Task-Manager", "WaGi - ERROR");
+                return;
             }
 
             //Administrator check
